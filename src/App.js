4ee51eb1581140926d4c370fc0555b6259b640ea
mohamed-chaouch <mohamedchaouch2212@gmail.com>
2024-06-06 23:01:36 +0100

    delete-files-unecessary

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
