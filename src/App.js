b39e8484b26b192f1283649cdff71a073fbc3a10
mohamed-chaouch <mohamedchaouch2212@gmail.com>
2024-06-06 23:27:38 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import './App.css';
+import Navbar from "./pages/navbar";
 
 function App() {
   return (
-    <div>
-      <header>
-        <img src="" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar />
   );
 }
 
---
+++ src/pages/navbar/index.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <div className="flex items-center">
+            <h1 className="flex-grow">Navbar</h1>
+            <h4>Hello</h4>
+        </div>
+    )
+}
+
+export default Navbar;
