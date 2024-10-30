2cd9f04ae59fe70400b01e9a7d5ac368f459646f
mohamed-chaouch <mohamedchaouch2212@gmail.com>
2024-10-30 19:27:12 +0100

    env mongodb

--- Backend/config/connect.js
+++ Backend/config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1:27017/Portfolio').then((res)=>{
-mongoose.connect("mongodb+srv://portfolio:portfolio@cluster0.oaggve9.mongodb.net/portfolio?retryWrites=true&w=majority&appName=Cluster0").then((res)=>{
+mongoose.connect(process.env.MongoDB_URL).then((res)=>{
     console.log('Connected to MongoDB successfully');
 }).catch((err)=>{
     console.log(err);
